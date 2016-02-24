7d2494404b8f6c2f3b2eb3eeb18f129d56b32d19
Stephen O'Kennedy <stephenoken@gmail.com>
2016-02-24 16:05:58 +0000

    "Can now server static files from public folder"
    ""

--- http-2/server.js
+++ http-2/server.js
@@ -3,33 +3,17 @@
 const server = require("http2");
 const fs = require("fs");
 const path = require("path");
+const nodeStatic = require('node-static');
 const contentType = 'text/html';
+
 const options = {
   key: fs.readFileSync(path.join(__dirname, '/certs/localhost.key')),
   cert: fs.readFileSync(path.join(__dirname, '/certs/localhost.crt'))
 };
+const file = new nodeStatic.Server('./../public');
 
 server.createServer(options, function(request, response) {
-  if(request.url.indexOf('.html') != -1){
-    response.setHeader('Content-Type', 'text/html');
-    response.end(fs.readFileSync(path.join(__dirname,'/../public/index.html')));
-  }
-
-  if(request.url.indexOf('.css') != -1){ //req.url has the pathname, check if it conatins '.csss'
-      console.log(fs.readdirSync(__dirname + '/../public/styles/'));
-      const files = fs.readdirSync(__dirname + '/../public/styles/');
-      fs.readFile(__dirname + '/../public/styles/styles.css', function (err, data) {
-        if (err) console.log(err);
-        response.setHeader('Content-Type', 'text/css');
-        response.end(data);
-      });
-  }
-  if(request.url.indexOf('.js') != -1){ //req.url has the pathname, check if it conatins '.js'
-
-      fs.readFile(__dirname + '/../public/scripts/script.js', function (err, data) {
-        if (err) console.log(err);
-        response.setHeader('Content-Type', 'text/javascript');
-        response.end(data);
-      });
-  }
+  request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
 }).listen(8080);
