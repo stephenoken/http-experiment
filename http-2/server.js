7be0c603e4cfcc256f308e2ed6725f4e29d95e91
Stephen O'Kennedy <stephenoken@gmail.com>
2016-02-24 16:45:24 +0000

    "Added log statements and can now server from static files"
    ""

--- http-2/server.js
+++ http-2/server.js
@@ -4,7 +4,13 @@ const server = require("http2");
 const fs = require("fs");
 const path = require("path");
 const nodeStatic = require('node-static');
+const morgan = require('morgan');
+const finalhandler = require('finalhandler');
+
 const contentType = 'text/html';
+
+// create "middleware"
+const logger = morgan('dev');
 
 const options = {
   key: fs.readFileSync(path.join(__dirname, '/certs/localhost.key')),
@@ -13,7 +19,11 @@ const options = {
 const file = new nodeStatic.Server('./../public');
 
 server.createServer(options, function(request, response) {
-  request.addListener('end', function () {
-        file.serve(request, response);
-    }).resume();
+  var done = finalhandler(request, response)
+  logger(request,response,(err)=>{
+     if (err) return done(err);
+     request.addListener('end', function () {
+       file.serve(request, response);
+     }).resume();
+  });
 }).listen(8080);
