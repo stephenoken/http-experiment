a4caa0d604c11a1e6d53b788ec067480ac0a5c08
Stephen O'Kennedy <stephenoken@gmail.com>
2016-03-16 14:34:54 +0000

    "Tested all the optimisations"
    ""

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var gulp = require('gulp');
 var gzip = require("gulp-gzip");
 var imagemin = require('gulp-imagemin');
 var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var sprity = require('sprity');
+var gulpif = require('gulp-if');
 
 gulp.task("scripts",()=>{
   return gulp.src('./public/scripts/*.js')
@@ -35,5 +38,27 @@ gulp.task("images-min",()=>{
 gulp.task("scripts-gzip",()=>{
   return gulp.src('./public/scripts/*.js')
     .pipe(gzip({append:false}))
+    .pipe(gulp.dest('./public/optimised'));
+});
+
+gulp.task("styles",()=>{
+  return gulp.src('./public/styles/**.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./public/optimised'));
+});
+
+gulp.task('sprites', function () {
+  return sprity.src({
+    src: './public/images/**/*.{png,jpg}',
+    style: './sprite.css'
+  })
+  .pipe(gulpif('*.png', gulp.dest('./public/optimised/'), gulp.dest('./public/optimised/')))
+});
+
+gulp.task("sprites-min",()=>{
+  return gulp.src('./public/images/**/*.png')
+    .pipe(imagemin({
+    		optimizationLevel: 7
+    }))
     .pipe(gulp.dest('./public/optimised'));
 });
